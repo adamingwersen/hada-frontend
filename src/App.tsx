85caf46773314ece4d21dd36e91cc917d46cac04
Adam Ingwersen Linnemann <adam@Adams-MacBook-Pro.local>
2022-05-25 16:44:13 +0200

    fix google auth -> lookup proto functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import "./App.css";
-
 import React, { useState } from "react";
+import {
+  Button as MButton,
+  TextInput,
+  Container,
+  Grid,
+  Code,
+} from "@mantine/core";
 
 import { useGoogleLogin } from "@react-oauth/google";
-
+// import { google } from "googleapis";
+// import { OAuth2Client } from "google-auth-library";
 import { TokenResponse } from "./constants/google-auth.types";
 
+// const CLIENT_ID =
+//   "815017879896-cuosdebbjmvlm3l41aevv4kqogi5nk6t.apps.googleusercontent.com";
+// const CLIENT_SECRET = "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";
+
 function App() {
+  // const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET);
+  const [emailInputvalue, setEmailInputvalue] = useState("Search email");
   const [userToken, setUserToken] = useState<string>();
+  const [specificUserData, setSpecificUserData] = useState<any>("None");
+
   const onLoginSuccess = (tokenResponse: TokenResponse) => {
     console.log(tokenResponse);
+    console.log(tokenResponse.access_token);
     const accessToken = tokenResponse.access_token;
     setUserToken(accessToken);
+    // oauth2Client.setCredentials(tokenResponse);
   };
 
   const login = useGoogleLogin({
@@ -22,23 +39,68 @@ function App() {
 
   const getUserList = async (token: string) => {
     const response = await fetch(
-      "https://admin.googleapis.com/admin/directory/v1/users",
+      "https://admin.googleapis.com/admin/directory/v1/users?customer=my_customer",
       {
         method: "GET",
-        mode: "no-cors",
-        headers: { Authorization: `Bearer ${token}`, body: "list" },
+        headers: { Authorization: `Bearer ${token}` },
       }
-    );
-    console.log(response);
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  };
+
+  const getSpecificUser = async (token: string, email: string) => {
+    const response = await fetch(
+      `https://admin.googleapis.com/admin/directory/v1/users/${email}`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+      .then(function (data) {
+        setSpecificUserData(data);
+      });
   };
 
   return (
     <div className="App">
-      <Button onClick={login} color="crimson" text="Login"></Button>
-      <Button
-        onClick={() => getUserList(userToken ?? "")}
-        text="Test authorized function"
-      ></Button>
+      <Container>
+        <Button onClick={login} color="crimson" text="Auth Me"></Button>
+        <Button
+          onClick={() => getUserList(userToken ?? "")}
+          // onClick={() => listUsers(oauth2Client)}
+          text="Test authorized function"
+        ></Button>
+      </Container>
+      <Grid>
+        <Grid.Col span={8}>
+          <TextInput
+            variant="default"
+            placeholder={emailInputvalue}
+            onChange={(event) => setEmailInputvalue(event.currentTarget.value)}
+          />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <MButton
+            onClick={() => getSpecificUser(userToken ?? "", emailInputvalue)}
+          >
+            Query User
+          </MButton>
+        </Grid.Col>
+      </Grid>
+      <Grid>
+        <Code block>{specificUserData}</Code>
+      </Grid>
     </div>
   );
 }
@@ -66,3 +128,20 @@ const Button = ({ onClick, text, color = "slateblue" }: ButtonProps) => {
     </div>
   );
 };
+
+type InputProps = {
+  value: string;
+  setValue: (value: string) => void;
+  text: string;
+};
+
+// const Input = ({ value, setValue, text }: InputProps) => {
+//   return (
+//     <input
+//       onChange={(inputValue) => setValue(inputValue)}
+//       type="text"
+//       id="name"
+//       name="name"
+//     />
+//   );
+// };
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const gClientId =
   "815017879896-cuosdebbjmvlm3l41aevv4kqogi5nk6t.apps.googleusercontent.com";
+const CLIENT_ID =
+  "815017879896-cuosdebbjmvlm3l41aevv4kqogi5nk6t.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
